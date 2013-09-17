73a419cc62946af7ea3c30c894f72c4c0a4d2098
Stefano Palazzo <stefano.palazzo@gmail.com>
2013-09-17 21:15:26 +0200

    Descriptions

--- setup.py
+++ setup.py
@@ -8,4 +8,18 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     test_suite="pcr.tests",
     install_requires=[],
+    description="Python 3 Cryptography Toolkit",
+    long_description=(
+        "A cryptography toolkit implemented in pure python 3. PCR includes "
+        "AES, CBC (mode of operation), Diffie-Hellman, HOPT (two-factor-"
+        "authentication), pbkdf2 (key derivation), RC4, and XTEA, as well "
+        "as some ancillary modules."),
+    platforms="all",
+    author="Stefano Palazzo",
+    author_email="stefano.palazzo@gmail.com",
+    url="https://github.com/sfstpala/pcr/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Security :: Cryptography"],
 )
